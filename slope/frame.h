21b5cae90d69b01b7eb0a9a7864726b81e4f49f1
Elvis Teixeira <elvis.teixeira@lsbd.ufc.br>
2015-03-13 13:22:33 -0300

    Added base frame

---
+++ slope/frame.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2015  Elvis Teixeira
+ *
+ * This source code is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This source code is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _SLOPE_FRAME_H_
+#define _SLOPE_FRAME_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _slope_frame slope_frame_t;
+
+void slope_frame_destroy (slope_frame_t *frame);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*_SLOPE_FRAME_H_*/
+
