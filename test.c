d84028ffaa7ef9c64c069d8c841dd716038f463a
Elvis Teixeira <elvis.teixeira@lsbd.ufc.br>
2015-03-13 13:22:58 -0300

    Update test app

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 #include <slope/slope.h>
 #include <math.h>
 
-#define N 100
+#define N 500
 
 
 static gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
@@ -23,21 +23,28 @@ static gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
 
 int main(int argc, char *argv[])
 {
-    double x1[N], y1[N];
+    double x[N], y1[N], y2[N];
     int k;
     for (k=0; k<N; k++) {
-        x1[k] = 0.1*k;
-        y1[k] = sin(x1[k]);
+        x[k] = (2.0*M_PI/N)*k;
+        y1[k] = sin(x[k]);
+        y2[k] = cos(x[k]);
     }
-    
+
     slope_data_t *sin_data =
         slope_xydata_create_simple(
-            x1, y1, N, "Sine",
+            x, y1, N, "Sine",
             SLOPE_BLUE, SLOPE_LINE);
-    
+
+    slope_data_t *cos_data =
+        slope_xydata_create_simple(
+            x, y2, N, "Cossine",
+            SLOPE_RED, SLOPE_LINE);
+
     slope_metrics_t *metrics = slope_xymetrics_create();
     slope_metrics_add_data(metrics, sin_data);
-    
+    slope_metrics_add_data(metrics, cos_data);
+
     slope_scene_t *scene = slope_scene_create();
     slope_scene_add_metrics(scene, metrics);
 
@@ -52,11 +59,11 @@ int main(int argc, char *argv[])
                      G_CALLBACK(on_draw), scene);
     gtk_widget_show_all(window);
     gtk_main();
-    
+
     slope_scene_write_to_png(scene, "figure.png", 600, 450); 
     slope_data_destroy(sin_data);
+    slope_data_destroy(cos_data);
     slope_metrics_destroy(metrics);
     slope_scene_destroy(scene);
     return 0;
 }
-
