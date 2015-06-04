a900000637d4b53087576dac72255882b32ab635
Elvis Teixeira <elvismtt@gmail.com>
2015-06-03 23:33:38 -0300

    Added vector

---
+++ slope/vector.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2015  Elvis Teixeira
+ *
+ * This source code is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This source code is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __SLOPE_VECTOR_H
+#define __SLOPE_VECTOR_H
+
+#include "slope/global.h"
+
+__SLOPE_BEGIN_DECLS
+
+/**
+ */
+typedef struct _slope_vector
+{
+    double *v;
+    int nalloc;
+    int size;
+}
+slope_vector_t;
+
+
+slope_public slope_vector_t* slope_vector_create (int size);
+
+slope_public void slope_vector_destroy (slope_vector_t *vector);
+
+slope_public void slope_vector_append (slope_vector_t *vector, double value);
+
+__SLOPE_END_DECLS
+
+#endif /* __SLOPE_VECTOR_H */
