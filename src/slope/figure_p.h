a8d6b1cea09712cd450d353477e75033548609aa
elvismt <elvismtt@gmail.com>
2017-04-05 08:57:32 -0300

    Started work on legend and updated license header

--- src/slope/figure_p.h
+++ src/slope/figure_p.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016  Elvis Teixeira
+ * Copyright (C) 2017  Elvis Teixeira
  *
  * This source code is free software: you can redistribute it
  * and/or modify it under the terms of the GNU Lesser General
